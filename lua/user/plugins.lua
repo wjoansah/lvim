e6071cec11f7a2e20d5189b2c0ba52948eeca44a
wjoansah <wonderful.owusu-ansah@amalitech.com>
2023-02-17 13:47:12 +0000

    install colorizer and catppuccin plugins

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -9,7 +9,22 @@ lvim.plugins = {
 	{
 		"xiyaowong/nvim-cursorword",
 		config = function()
-      require("user.cursorword")
+			require("user.cursorword")
+		end,
+	},
+	{ "catppuccin/nvim", config = function() end },
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup({ "*" }, {
+				RGB = true, -- #RGB hex codes
+				RRGGBB = true, -- #RRGGBB hex codes
+				RRGGBBAA = true, -- #RRGGBBAA hex codes
+				rgb_fn = true, -- CSS rgb() and rgba() functions
+				hsl_fn = true, -- CSS hsl() and hsla() functions
+				css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+				css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+			})
 		end,
 	},
 }
