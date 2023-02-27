de54530b8f83d81a7b8386bf4c35b4ccf6f96f92
wjoansah <wonderful.owusu-ansah@amalitech.com>
2023-02-27 07:56:36 +0000

    add nvim spectre to which key

---
+++ lua/user/whichkey.lua
@@ -0,0 +1,6 @@
+lvim.builtin.which_key.mappings["r"] = {
+	name = "Replace",
+	r = { "<cmd>lua require('spectre').open()<cr>", "Replace" },
+	w = { "<cmd>lua require('spectre').open_visual({select_word=true})<cr>", "Replace Word" },
+	f = { "<cmd>lua require('spectre').open_file_search()<cr>", "Replace Buffer" },
+}
