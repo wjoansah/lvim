b8af7e994945e4b0406cd0cb6f1529654cc7577b
wjoansah <wonderful.owusu-ansah@amalitech.com>
2023-04-11 08:47:28 +0000

    add whichkey keybindings for harpoon plugin

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -4,3 +4,27 @@ lvim.builtin.which_key.mappings["r"] = {
 	w = { "<cmd>lua require('spectre').open_visual({select_word=true})<cr>", "Replace Word" },
 	f = { "<cmd>lua require('spectre').open_file_search()<cr>", "Replace Buffer" },
 }
+
+local m_opts = {
+	mode = "n", -- NORMAL mode
+	prefix = "m",
+	buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+	silent = true, -- use `silent` when creating keymaps
+	noremap = true, -- use `noremap` when creating keymaps
+	nowait = true, -- use `nowait` when creating keymaps
+}
+
+local status_ok, which_key = pcall(require, "which-key")
+if not status_ok then
+	return
+end
+
+local m_mappings = {
+	m = { '<cmd>lua require("harpoon.mark").add_file()<cr>', "Harpoon" },
+	["."] = { '<cmd>lua require("harpoon.ui").nav_next()<cr>', "Harpoon Next" },
+	[","] = { '<cmd>lua require("harpoon.ui").nav_prev()<cr>', "Harpoon Prev" },
+	s = { "<cmd>Telescope harpoon marks<cr>", "Search Files" },
+	[";"] = { '<cmd>lua require("harpoon.ui").toggle_quick_menu()<cr>', "Harpoon UI" },
+}
+
+which_key.register(m_mappings, m_opts)
