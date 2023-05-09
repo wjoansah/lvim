e10f7ce150fa2939dd5a6582310cdd65ff7f0267
wjoansah <wonderful.owusu-ansah@amalitech.com>
2023-05-09 11:51:31 +0000

    install nvim-surround plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -36,4 +36,14 @@ lvim.plugins = {
 	{
 		"mrjones2014/nvim-ts-rainbow",
 	},
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
 }
